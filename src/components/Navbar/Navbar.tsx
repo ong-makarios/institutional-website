62c19ff6deb44b44eda07a16b9d5a2ad398aeafa
Leonardo Giraldi Moreno Giuranno <leogiraldimg@gmail.com>
2023-03-07 08:49:36 -0300

    refactor: encapsulate navbar elements

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,34 +1,14 @@
 import React from "react";
 import "./Navbar.scss";
+import NavbarCollapseButton from "./NavbarCollapseButton";
+import NavbarItems from "./NavbarItems";
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-red-orange" role="navigation">
       <div className="container">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#main-navbar"
-          aria-controls="main-navbar"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="main-navbar">
-          <ul className="navbar-nav">
-            <li className="nav-item navbar-item">
-              <a
-                className="nav-link t-white-alabaster"
-                aria-current="page"
-                href="#"
-              >
-                Home
-              </a>
-            </li>
-          </ul>
-        </div>
+        <NavbarCollapseButton />
+        <NavbarItems />
       </div>
     </nav>
   );
---
+++ src/components/Navbar/NavbarItems.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavbarButtonProps } from "../../types/props";
+import NavbarButton from "./NavbarButton";
+
+const buttons: Array<NavbarButtonProps> = [{ name: "Home", path: "/" }];
+
+const NavbarItems: React.FC = () => {
+  return (
+    <div className="collapse navbar-collapse" id="main-navbar">
+      <ul className="navbar-nav">
+        {buttons.map((button) => (
+          <NavbarButton name={button.name} path={button.path} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default NavbarItems;
